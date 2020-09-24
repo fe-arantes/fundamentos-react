e06251d11530a558d8038e7581fcd68a99d0d593
Felipe Arantes <fe.arantes@outlook.com>
2020-09-24 11:48:36 -0300

    Day 2

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+import Primeiro from './components/basic/Primeiro'
+
+const tag = <strong>Olá React</strong>
+
+ReactDOM.render(
+        <div>
+            <Primeiro></Primeiro>
+        </div>, 
+        document.getElementById('root')
+)
+
+
+
